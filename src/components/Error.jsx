5889c4aa863eed6cf714c11d6474276460337573
DaniM0613 <wwh.rj.92@gmail.com>
2023-02-06 22:34:10 -0600

    Primer Commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+
+
+const Error = ({children}) => {
+  return (
+     <div className="bg-purple-900 text-center text-white p-3 uppercase font-bold rounded-md mb-3">
+                    {children}   
+                </div>
+  )
+}
+
+export default Error
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Header(){
+     return (
+         
+          <h1 className="font-black text-4xl text-center md:w-2/3 mx-auto">
+            Listado de Pacientes {""}
+            <span className="text-purple-900"> Veterinaria</span>
+
+          </h1>
+          
+          
+
+    
+        
+    )
+}
+
+export default Header;
